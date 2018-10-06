b4027e6a819ddfa7ca8524070ff68fdcdf274917
Tom Meagher <thom.meagher@gmail.com>
2018-10-05 23:15:49 -0700

    Get firebase function up and running

--- src/api.js
+++ src/api.js
@@ -1,11 +1,15 @@
-const REGION = 'us-central'
+const REGION = 'us-central1'
 const PROJECT_ID = 'paytweetb'
-const URL = `https://${REGION}-${PROJECT_ID}.cloudfunctions.net`
+const URL =
+    process.env.NODE_ENV === 'production'
+        ? `https://${REGION}-${PROJECT_ID}.cloudfunctions.net`
+        : 'http://localhost:5001/paytweetb/us-central1'
 
 export default {
     subscribe(data) {
-        return fetch('http://localhost:5001/paytweetb/us-central1/subscribe', {
+        return fetch(`${URL}/subscribe`, {
             method: 'POST',
+            mode: 'no-cors',
             body: JSON.stringify(data),
         }).then(response => response.json())
     },
