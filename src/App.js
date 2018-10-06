c0bfc233106e2a4f2480a3bfdc1436a779575423
Athyuttam Eleti <athyuttamre@gmail.com>
2018-10-06 10:38:00 -0700

    Add payment, simplify cors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { StripeProvider, Elements } from "react-stripe-elements";
 
 import HomePage from "./HomePage.js";
 import CreatorPage from "./CreatorPage.js";
@@ -10,13 +11,17 @@ import "./App.css";
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/creator" component={CreatorPage} />
-          <Route exact path="/:creatorUsername" component={SubscribePage} />
-        </Switch>
-      </Router>
+      <StripeProvider apiKey="pk_test_wL4QYRyypDqOwAQk2pdFPS1g">
+        <Elements>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/creator" component={CreatorPage} />
+              <Route exact path="/:creatorUsername" component={SubscribePage} />
+            </Switch>
+          </Router>
+        </Elements>
+      </StripeProvider>
     );
   }
 }
--- src/api.js
+++ src/api.js
@@ -1,15 +1,15 @@
-const REGION = 'us-central1'
-const PROJECT_ID = 'paytweetb'
+const REGION = "us-central1";
+const PROJECT_ID = "paytweetb";
 const URL =
-    process.env.NODE_ENV === 'production'
-        ? `https://${REGION}-${PROJECT_ID}.cloudfunctions.net`
-        : `http://localhost:5001/${PROJECT_ID}/${REGION}`
+  process.env.NODE_ENV === "production"
+    ? `https://${REGION}-${PROJECT_ID}.cloudfunctions.net`
+    : `http://localhost:5000/${PROJECT_ID}/${REGION}`;
 
 export default {
-    subscribe(data) {
-        return fetch(`${URL}/subscribe`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-        }).then(response => response.json())
-    },
-}
+  subscribe(data) {
+    return fetch(`${URL}/subscribe`, {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then(response => response.json());
+  }
+};
